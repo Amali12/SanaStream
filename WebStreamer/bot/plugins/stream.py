01036e9b84b942f7f2caeacfb02cd8534a85f842
Amali12 <80261040+Amali12@users.noreply.github.com>
2023-10-12 12:29:36 +0545

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -35,25 +35,26 @@ async def media_receive_handler(_, m: Message):
     short_link = f"{Var.URL}{file_hash}{log_msg.id}"
     logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     try:
-    await log_msg.reply_text(
-        text=f"<b>⚡Short Url:</b> <a href='{short_link}'>{short_link}</a>\n\n <b>⚡Long Url:</b> <a href='{stream_link}'>{stream_link}</a>",
-        quote=True,
-        parse_mode=ParseMode.HTML,
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("⚡ Open Link ⚡", url=short_link)]]
-        ),
-    )
-    await m.reply_text(
-        text=f"<b>⚡Short Url:</b> <a href='{short_link}'>{short_link}</a>\n\n <b>⚡Long Url:</b> <a href='{stream_link}'>{stream_link}</a>",
-        quote=True,
-        parse_mode=ParseMode.HTML,
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("⚡ Open Link ⚡", url=short_link)]]
-        ),
-    )
-except errors.ButtonUrlInvalid:
-    await m.reply_text(
-        text=f"<b>⚡Short Url:</b> <a href='{short_link}'>{short_link}</a>\n\n <b>⚡Long Url:</b> <a href='{stream_link}'>{stream_link}</a>",
-        quote=True,
-        parse_mode=ParseMode.HTML
-    )
+        await log_msg.reply_text(
+            text=f"<b>⚡Short Url:</b> <code>{short_link}</code>\n\n <b>⚡Long Url:</b> <code>{stream_link}</code>",
+            quote=True,
+            parse_mode=ParseMode.MARKDOWN
+        )
+        await m.reply_text(
+            text="<b>⚡Short Url:</b> <code>{}</code>\n\n <b>⚡Long Url:</b> <code>{}</code>".format(
+                short_link, stream_link
+            ),
+            quote=True,
+            parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Open Link", url=short_link)]]
+            ),
+        )
+    except errors.ButtonUrlInvalid:
+        await m.reply_text(
+            text="<b>⚡Short Url:</b> <code>{}</code>\n\n <b>⚡Long Url:</b> <code>{}</code>".format(
+                short_link, stream_link
+            ),
+            quote=True,
+            parse_mode=ParseMode.HTML,
+        )
